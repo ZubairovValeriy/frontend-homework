75ab10956aced2166e23a584c9c3b03d1ca2328c
ZubairovValeriy <ValeriyIU5-12@yandex.ru>
2017-08-28 17:22:43 +0300

    Add files via upload

--- tests/plainify.js
+++ tests/plainify.js
@@ -41,5 +41,40 @@ QUnit.module('Тестируем функцию plainify', function () {
 		};
 
 		assert.deepEqual(plainify(nested2), plain2);
+		
+		const nested3 = {
+			property: 'work',
+			deep: {
+				foo: 'bar',
+				baz: 42
+			}
+		};
+		
+		const plain3 = {
+			'property': 'work',
+			'deep.foo': 'bar',
+			'deep.baz': 42
+		};
+		
+		assert.deepEqual(plainify(nested3), plain3);
+		
+		const nested4 = {
+			property: 'work',
+			deep: {
+				foo: 'bar',
+				baz: 42
+			},
+			property1: 'work1'
+		};
+		
+		const plain4 = {
+			'property': 'work',
+			'deep.foo': 'bar',
+			'deep.baz': 42,
+			'property1': 'work1'
+		};
+		
+		assert.deepEqual(plainify(nested4), plain4);
+		
 	});
 });

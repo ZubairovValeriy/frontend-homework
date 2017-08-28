0e340775a5cd07766936af7e062ed5a625724fa2
ZubairovValeriy <ValeriyIU5-12@yandex.ru>
2017-08-28 16:50:52 +0300

    Add files via upload

--- source/plainify.js
+++ source/plainify.js
@@ -1,41 +1,30 @@
 'use strict';
-function pl(obj, prop1) {
+function pl(obj, prop1, result) {
 	
 	for(const prop in obj) {
 		var pr = prop1 + "." + prop;
 		const value = obj[prop];
 		if (typeof value === 'object') {
-			pl(value, pr);
+			pl(value, pr, result);
 		}
 		else {
 			result[pr.toString()] = value
 		}
 	}
-	return
+	return result
 }
-function copy(obj) {
-	const res = {};
-	for(const prop in obj){
-		res[prop] = obj[prop];
-	}
-	return res;
-}
-const result = {}
-function plainify(obj) {
+
+function plainify(obj, prop) {
+	var result = {}
 	for(const prop in obj) {
 		const value = obj[prop];
 		var prop1 = prop;
 		if (typeof value === 'object') {
-			pl(value, prop1);
+			pl(value, prop1, result);
 		}
 		else {
 			result[prop] = value
 		}
 	}
-	
-	const res1 = copy(result);
-	for(const p in result){
-		delete result[p];
-	}
-	return res1;
+	return result;
 }
